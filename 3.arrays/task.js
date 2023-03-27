2e83cb7f706727b0897c3b941f143b898781a5a5
Lev <ya.abc2001@ya.ru>
2023-03-27 13:22:53 +0500

    Третье задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every((el, idx) => arr1.length === arr2.length && el === arr2[idx]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users.
+    filter(user => user.gender === gender).
+    map(user => user.age).
+    reduce((average, userAge, index, array) => average + userAge / array.length, 0);
 }
